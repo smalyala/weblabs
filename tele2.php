c8d325891253c69525b362f2d6095fadf7f0c9f8
smalyala <sahith.malyala325@gmail.com>
2014-11-25 18:17:25 -0500

    php from command line

---
+++ tele2.php
@@ -0,0 +1,35 @@
+<?php 
+
+	// function to swap the char at pos $i and $j of $str.
+	function swap(&$datum,$i,$j) {
+	    $temp = $datum[$i];
+	    $datum[$i] = $datum[$j];
+	    $datum[$j] = $temp;
+	} 
+
+	// function to generate and print all N! permutations of $str. (N = strlen($str)).
+	function solve($datum,$i,$n) {
+	   if ($i == $n) {
+	   		global $arr;
+	   		if (!in_array($datum, $arr)) {
+	    		array_push($arr, $datum);
+	    	}
+	    }
+	   else {
+	        for ($j = $i; $j < $n; $j++) {
+	          swap($datum,$i,$j);
+	          solve($datum, $i+1, $n);
+	          swap($datum,$i,$j); // backtrack.
+	       }
+	   }
+	}
+	if (isset($argv[1])) {
+		$datum = $argv[1];
+		$arr = [];
+		solve($datum, 0,strlen($datum)); // call the function.
+		echo 'Permutations: '. count($arr);
+		echo '<pre>'; 
+		print_r($arr); 
+		echo '</pre>';
+	}
+?> 
