66492bb81ccbba5b97dbc88949105002a77980e8
smalyala <sahith.malyala325@gmail.com>
2014-12-02 18:55:21 -0500

    only one visit time in file

--- index.php
+++ index.php
@@ -2,33 +2,19 @@
 <html>
 <head>
 <title>Previous Visit</title>
-<script>
-setInterval(function() {
-    var currentTime = new Date ( );    
-    var currentHours = currentTime.getHours ( );   
-    var currentMinutes = currentTime.getMinutes ( );   
-    var currentSeconds = currentTime.getSeconds ( );
-    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;   
-    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;    
-    var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";    
-    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;    
-    currentHours = ( currentHours == 0 ) ? 12 : currentHours;    
-    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-    document.getElementById("timer").innerHTML = currentTimeString;
-}, 1000);
-</script>
 </head>
 <body>
 
 <?php  
 
 $arr = file('visits.txt');
-echo "Last visit at " . end($arr);
+echo "Last visit: " . end($arr);
+file_put_contents("visits.txt", "");
 
 $file = 'visits.txt';
 $current = file_get_contents($file);
 $current .= date("g:i:s");
-echo "<br> Current time " . date("g:i:s");
+echo "<br> Current time: " . date("g:i:s");
 file_put_contents($file, $current);
 
 $handle = fopen("counter.txt", "r"); 
@@ -38,7 +24,7 @@ if(!$handle) {
 else { 
 	$counter = (int ) fread($handle,20); 
 	fclose ($handle); $counter++; 
-	echo" <br> <strong> You are the ". $counter . " visitor </strong> " ; 
+	echo" <br> You are the ". $counter . " visitor" ; 
 	$handle = fopen("counter.txt", "w" ); 
 	fwrite($handle,$counter) ; 
 	fclose ($handle) ;
@@ -46,8 +32,6 @@ else {
 
 ?>
 <br>
-Current time:
-<div id="timer"></div>
 
 </body>
 </html>
