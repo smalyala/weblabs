d9383e0f5d5cf1a77f038e2a5bdf6cb64bb5180c
Sahith Malyala <sahith.malyala325@gmail.com>
2014-12-04 11:41:04 -0500

    Create search.php

---
+++ search.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+	<form name="f" method="GET" action="search.php">
+	Search Amazon:
+	<input type="text" name="search">
+		<button type="submit" accesskey="s"><u>S</u>earch</button>
+	</form>
+
+</body>
+
+</html>
