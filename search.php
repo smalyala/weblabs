686f0e6a40a032321bd450aa7c9b7b4a8c11d5e4
Sahith Malyala <sahith.malyala325@gmail.com>
2014-12-04 12:03:16 -0500

    parsing amazon

--- search.php
+++ search.php
@@ -3,11 +3,23 @@
 
 <body>
 
-	<form name="f" method="GET" action="search.php">
+	<form name="f" method="POST" action="search.php">
 	Search Amazon:
 	<input type="text" name="search">
 		<button type="submit" accesskey="s"><u>S</u>earch</button>
 	</form>
+
+	<?php
+	include('simple_html_dom.php');
+	if (isset($_POST["search"])) {
+		$search = $_POST["search"];
+		$url = "http://www.amazon.com/s/ref=nb_sb_noss_2/182-8477515-7038932?url=search-alias%3Daps&field-keywords=".$search;
+		$html = file_get_html($url);
+		foreach($html->find('div') as $element) {
+       		echo $element->src . '<br>';
+		}
+	}
+	?>
 
 </body>
 
