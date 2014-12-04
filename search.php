e46148710ddc8c09887ef39f49cc6827310c8c9e
Sahith Malyala <sahith.malyala325@gmail.com>
2014-12-04 12:09:11 -0500

    parser working

--- search.php
+++ search.php
@@ -14,9 +14,10 @@
 	if (isset($_POST["search"])) {
 		$search = $_POST["search"];
 		$url = "http://www.amazon.com/s/ref=nb_sb_noss_2/182-8477515-7038932?url=search-alias%3Daps&field-keywords=".$search;
+		echo $url;
 		$html = file_get_html($url);
 		foreach($html->find('div') as $element) {
-       		echo $element->src . '<br>';
+       		echo $element . '<br>';
 		}
 	}
 	?>
