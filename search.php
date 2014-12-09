803a906ec43a7cd7f432111af87aab73ea310e1a
Sahith Malyala <sahith.malyala325@gmail.com>
2014-12-09 10:59:19 -0500

    display in table

--- search.php
+++ search.php
@@ -15,12 +15,22 @@
 		$search = $_POST["search"];
 		$url = "http://www.amazon.com/s/ref=nb_sb_noss_2/182-8477515-7038932?url=search-alias%3Daps&field-keywords=".$search;
 		$html = file_get_html($url);
+		$j = 0;
+		$tbl = "<table>";
 		for ($i = 0; $i < 15; $i++) {
-			foreach($html->find('li[id=result_'.$i.']') as $element) {
-	       		echo $element . '<br>';
+			if ($j % 3 == 0) {
+				$arr = [];
 			}
-			echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+			foreach($html->find('li[id=result_'.$i.']') as $element) {
+	       		array_push($arr, $element);
+			}
+			if ($j % 3 == 0) {
+				$tbl .= "<tr><td>" . $arr[0] . "</td><td>" . $arr[1] . "</td></tr>";
+			}
+			$j++;
 		}
+		$tbl .= "</table>";
+		echo $tbl;
 	}
 	?>
 
